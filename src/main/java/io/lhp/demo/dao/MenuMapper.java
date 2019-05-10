36e74a5443f61580df76894198071851df98dff3
a13269756843 <984171281@qq.com>
2019-05-10 16:36:49 +0800

    add application and generatorConfig file

--- src/main/java/io/lhp/demo/dao/MenuMapper.java
+++ src/main/java/io/lhp/demo/dao/MenuMapper.java
@@ -4,7 +4,7 @@ import io.lhp.demo.po.Menu;
 
 public interface MenuMapper {
     int deleteByPrimaryKey(Integer menuId);
-
+    int delete(Menu menuid);
     int insert(Menu record);
 
     int insertSelective(Menu record);
